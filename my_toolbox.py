775a294b58530caa8c781689703a227b5300cfb9
Massy BOURENNANI <cm_bourennani@esi.dz>
2017-11-11 15:41:13 +0100

    using pipelines

---
+++ my_toolbox.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+def save_submission(file_name, y_test):
+    submission_df = pd.DataFrame({"Id": range(1, len(y_test) + 1),
+                                  "prediction": y_test})
+    submission_df.to_csv(tile_name)

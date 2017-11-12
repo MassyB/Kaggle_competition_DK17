6269f00f10e78f48942a7d6f1e7a6fa6fac4409f
Massy BOURENNANI <cm_bourennani@esi.dz>
2017-11-12 04:39:22 +0100

    g_boost 0596

--- my_toolbox.py
+++ my_toolbox.py
@@ -3,4 +3,4 @@ import pandas as pd
 def save_submission(file_name, y_test):
     submission_df = pd.DataFrame({"Id": range(1, len(y_test) + 1),
                                   "prediction": y_test})
-    submission_df.to_csv(tile_name)
+    submission_df.to_csv(file_name)
